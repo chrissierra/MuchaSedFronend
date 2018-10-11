f775bc9273537b9423b704f91682f842dd7744d9
chrissierra <christopher.sierra@usach.cl>
2018-10-11 12:31:13 -0300

    asdf

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -5,8 +5,8 @@ class Conexion{
 	public function conectar(){
 
 		$link = new PDO("mysql:host=localhost;dbname=ecommerce",
-						"root",
-						"",
+						"chris",
+						"jazzmaster",
 						array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 		                      PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
 						);
